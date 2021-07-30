9f939ad2035a8f871d52b4b9a07b251aa3b09548
Kag01 <keshav.agarwal@students.iiit.ac.in>
2021-07-30 15:51:50 +0530

    Updated code

--- a1.m
+++ a1.m
@@ -1,8 +1,23 @@
-% finding
+% finding the ascii value of the char entered and then increamenting the
+% respective index in th storasge array. then using all the values, find
+% the probability distribution and make a hamming code for it. total
+% charcters:
+% a 128X128 matrix which have 1 where the column no has the row no as its
+% children or is itsel. all M[i,i]=1 initially.
 
+fi=fopen("File__3.txt",'r');
 
-filename=input('Enter the name of the 1st source txt file:');
+a=fscanf(fi,"%c");
 
-a=['a','b','c','d'];
-b=ones(2,4);
-b(1,:)=a;
+asc=double(a);
+
+storage=zeros(1,128);
+
+for z=1:length(asc)
+    storage(asc(z)+1)=storage(asc(z)+1)+1;
+end
+
+total_char=sum(storage);
+pd=storage./total_char;
+
+length=zeros(1,128);
